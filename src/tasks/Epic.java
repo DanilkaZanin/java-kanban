0fec2ddb23655111be25e4ced35e44acad99016a
danila <y-zelenov@mail.ru>
2024-03-03 13:21:56 +0300

    Удалены лишние конструкторы, дописаны комментарии

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -3,44 +3,13 @@ package tasks;
 import status.Status;
 import java.util.ArrayList;
 
-/*
-Для каждой подзадачи известно, в рамках какого эпика она выполняется.
-Каждый эпик знает, какие подзадачи в него входят.
-Завершение всех подзадач эпика считается завершением эпика.*/
 public class Epic extends Task {
     ArrayList<Subtask> subtasks;
-    public Epic(String name, String description, Status status) {
-        super(name, description, status);
-        subtasks = new ArrayList<>();
-    }
 
-    public Epic(String name, String description, Status status, ArrayList<Subtask> subtasks) {
-        super(name, description, status);
-        this.subtasks = subtasks;
-    }
-
-    public Epic(String name, String description, Status status, Subtask subtask) {
-        super(name, description, status);
-
-        subtasks = new ArrayList<>();
-        this.subtasks.add(subtask);
-    }
-
+    /** Для создания пустого эпика */
     public Epic(String name, String description) {
         super(name, description, null);
         subtasks = new ArrayList<>();
-    }
-
-    public Epic(String name, String description, ArrayList<Subtask> subtasks) {
-        super(name, description, null);
-        this.subtasks = subtasks;
-    }
-
-    public Epic(String name, String description, Subtask subtask) {
-        super(name, description, null);
-
-        subtasks = new ArrayList<>();
-        this.subtasks.add(subtask);
     }
 
     /** Конструктор для изменения статуса */
@@ -48,15 +17,15 @@ public class Epic extends Task {
         super(epic.getName(), epic.getDescription(), status);
     }
 
-/*    * Добавление подзадачи в задачу
+    /** Добавление подзадачи в задачу */
     public void setSubtask(Subtask subtask){
         subtasks.add(subtask);
     }
 
-    * Добавление одной подзадачи
+    /** Добавление одной подзадачи */
     public void setSubtasks(ArrayList<Subtask> subtasks) {
         this.subtasks = subtasks;
-    }*/
+    }
 
     /** Получение списка подзадач */
     public ArrayList<Subtask> getSubtasks() {
