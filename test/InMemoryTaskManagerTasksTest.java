51215e10b662c0377a83c5a44d70054b33fbe582
danila <y-zelenov@mail.ru>
2024-03-15 20:53:47 +0300

    Добавлены тесты для Task

---
+++ test/InMemoryTaskManagerTasksTest.java
@@ -0,0 +1,90 @@
+import code.manager.InMemoryTaskManager;
+import code.manager.TaskManager;
+import code.status.Status;
+import code.tasks.Task;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/** Что должны делать таски?
+ * Возвращать список.
+ * Возвращать по id.
+ * Удалять все задачи.
+ * Получать задачу.
+ * Добавлять задачу.
+ * Обновлять задачу.
+ * Удалять задачу.
+ * */
+
+class InMemoryTaskManagerTasksTest {
+    private TaskManager taskManager;
+    private Task t1;
+    private Task t2;
+
+    @BeforeEach
+    public void preparing(){
+        taskManager = new InMemoryTaskManager();
+
+        t1 = new Task("Задача1", "Описание1", Status.NEW);
+        t2 = new Task("Задача2", "Описание2", Status.NEW);
+
+        taskManager.setTask(t1);
+        taskManager.setTask(t2);
+    }
+
+    @Test
+    public void shouldReturnArrayOfTasks() {
+        ArrayList<Task> tasks = taskManager.getTasks();
+        ArrayList<Task> tasks1 = new ArrayList<>(Arrays.asList(t1,t2));
+
+        assertEquals(tasks, tasks1);
+    }
+    @Test
+    public void shouldClearAllTasks() {
+        taskManager.deleteTasks();
+
+        ArrayList<Task> tasks = taskManager.getTasks();
+
+        assertEquals(tasks.size(), 0);
+    }
+
+    @Test
+    public void shouldGetTaskFromId() {
+        Task task = taskManager.getTask(t1.getId());
+
+        assertEquals(task, t1);
+    }
+
+    @Test
+    public void shouldAddOneMoreTask() {
+        Task task = new Task("AAA", "bbb", Status.IN_PROGRESS);
+        taskManager.setTask(task);
+
+        ArrayList<Task> tasks = taskManager.getTasks();
+
+        assertEquals(tasks.size(), 3);
+        assertEquals(tasks.get(2), task);
+    }
+
+    @Test
+    public void shouldUpdateTask() {
+        Task task = new Task("Задача1", "Описание1", Status.IN_PROGRESS);
+        task.setStatus(Status.IN_PROGRESS);
+        taskManager.updateTask(task);
+
+        assertEquals(taskManager.getTask(task.getId()).getStatus(), Status.IN_PROGRESS);
+    }
+
+    @Test
+    public void shouldRemoveTask() {
+        taskManager.deleteTask(t1.getId());
+
+        assertNull(taskManager.getTask(t1.getId()));
+    }
+}
