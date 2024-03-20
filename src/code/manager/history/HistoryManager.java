bc1dd211c6aef7fd6400d6e697cdcbc905b4eeec
danila <y-zelenov@mail.ru>
2024-03-20 13:35:53 +0300

    Сделан интерфейс в качестве возвращаемого типа. Переделан метод add

--- src/code/manager/history/HistoryManager.java
+++ src/code/manager/history/HistoryManager.java
@@ -2,17 +2,17 @@ package code.manager.history;
 
 import code.tasks.Task;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /** Интерфейс для управления историей просмотров */
 public interface HistoryManager {
     /** Для добавления задачи в менеджер историй.
      * @param task - задача, которую требуется добавить.
      * */
-    void add(Task task);
+    <T extends Task> void add(T task) ;
 
     /** Для получения истории последних задач.
      * @return список задач.
      * */
-    ArrayList<Task> getHistory();
+    List<Task> getHistory();
 }
--- src/code/manager/history/InMemoryHistoryManager.java
+++ src/code/manager/history/InMemoryHistoryManager.java
@@ -1,27 +1,34 @@
 package code.manager.history;
 
+import code.tasks.Epic;
+import code.tasks.Subtask;
 import code.tasks.Task;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final ArrayList<Task> taskList;
+    private final List<Task> taskList;
 
     public InMemoryHistoryManager() {
         taskList = new ArrayList<>(10);
     }
 
-    @Override
-    public void add(Task task) {
+    public <T extends Task> void add(T task) {
         if(taskList.size() > 10) {
             taskList.remove(0);
         }
 
-        taskList.add(new Task(task));
+        if(task instanceof Subtask)
+            taskList.add(new Subtask((Subtask) task));
+        else if(task instanceof Epic)
+            taskList.add(new Epic((Epic) task));
+        else
+            taskList.add(new Task(task));
     }
 
     @Override
-    public ArrayList<Task> getHistory() {
+    public List<Task> getHistory() {
         return taskList;
     }
 }
